9521dadab603d654e1fc450991397037c8ad4aa5
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-06-29 23:40:59 +0200

    wip

--- page-templates/home.php
+++ page-templates/home.php
@@ -9,7 +9,7 @@
 
 <div class="bg-geel flex items-center w-screen h-[calc(70vh-98px)] md:h-[calc(100vh-112px)] relative overflow-hidden bg-cover bg-center" style="background-image: url('');">
     <video class="w-full h-full object-cover absolute top-0 right-0" autoplay="" loop="" muted="" playsinline="">
-        <source src="/wp-content/themes/dogcarexxl/img/local/test3.mp4">
+        <source src="/wp-content/themes/dogcare/img/local/test3.mp4">
     </video>
     <div class="absolute top-0 left-0 right-0 bottom-0" style="box-shadow: rgb(0 0 0 / 10%) 0 0 0 100vw inset;"></div>
     <div class="container z-10">
@@ -26,12 +26,12 @@
     </div>
     <div class="bg-one absolute bottom-2 left-2 right-2 h-8 w-[calc(100vw-40px)]">
         <div class="grid grid-cols-3 md:grid-cols-6 gap-2 h-8">
-            <div class="col-span-1 flex items-center justify-center grayscale opacity-60 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcarexxl/img/local/hery.png" alt=""></div>
-            <div class="col-span-1 flex items-center justify-center grayscale opacity-40 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcarexxl/img/local/renske.png" alt=""></div>
-            <div class="col-span-1 flex items-center justify-center grayscale opacity-100 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcarexxl/img/local/edgard.png" alt=""></div>
-            <div class="col-span-1 hidden md:flex items-center justify-center grayscale opacity-40 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcarexxl/img/local/pawr.png" alt=""></div>
-            <div class="col-span-1 hidden md:flex items-center justify-center grayscale opacity-40 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcarexxl/img/local/trixie.png" alt=""></div>
-            <div class="col-span-1 hidden md:flex items-center justify-center grayscale opacity-40 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcarexxl/img/local/lilys.png" alt=""></div>
+            <div class="col-span-1 flex items-center justify-center grayscale opacity-60 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcare/img/local/hery.png" alt=""></div>
+            <div class="col-span-1 flex items-center justify-center grayscale opacity-40 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcare/img/local/renske.png" alt=""></div>
+            <div class="col-span-1 flex items-center justify-center grayscale opacity-100 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcare/img/local/edgard.png" alt=""></div>
+            <div class="col-span-1 hidden md:flex items-center justify-center grayscale opacity-40 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcare/img/local/pawr.png" alt=""></div>
+            <div class="col-span-1 hidden md:flex items-center justify-center grayscale opacity-40 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcare/img/local/trixie.png" alt=""></div>
+            <div class="col-span-1 hidden md:flex items-center justify-center grayscale opacity-40 hover:grayscale-0 hover:opacity-100 duration-300"><img class="h-6" src="/wp-content/themes/dogcare/img/local/lilys.png" alt=""></div>
         </div>
     </div>
 
