74ebe3d8dd4a845a29be1c937a3a5360a4aeacb6
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-06-30 11:34:26 +0200

    wip

--- page-templates/home.php
+++ page-templates/home.php
@@ -7,13 +7,13 @@
  get_header(); ?>
  
 
-<div class="bg-geel flex items-center w-screen h-[calc(70vh-98px)] md:h-[calc(100vh-112px)] relative overflow-hidden bg-cover bg-center" style="background-image: url('');">
+<div class="bg-geel flex items-center w-screen h-[calc(85vh-98px)] md:h-[calc(100vh-112px)] relative overflow-hidden bg-cover bg-center" style="background-image: url('');">
     <video class="w-full h-full object-cover absolute top-0 right-0" autoplay="" loop="" muted="" playsinline="">
         <source src="/wp-content/themes/dogcare/img/local/test3.mp4">
     </video>
     <div class="absolute top-0 left-0 right-0 bottom-0" style="box-shadow: rgb(0 0 0 / 10%) 0 0 0 100vw inset;"></div>
     <div class="container z-10">
-        <div class="flex flex-col absolute md:relative bottom-8 left-2 md:bottom-0 md:left-0 right-2 md:justify-center items-center mb-5">
+        <div class="flex flex-col md:justify-center items-center mb-5">
             <h1 class="text-28 leading-28 md:text-40 md:leading-60 mb-3 text-one font-titel">Dé online honden drogist</h1>
             <div class="grid grid-cols-2 md:grid-cols-5 gap-[15px] w-full">
                 <a href="/producten/categorie-mondverzorging/" class="col-span-1 flex items-center justify-center py-1 text-15 bg-white text-grijs hover:opacity-[.8] duration-300">Mondverzorging</a>
